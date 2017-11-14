7906a6ac3614882a1b6172dd82bd5d5b1e072310
Rebeca Medeiros <rebeca.andrade.medeiros@gmail.com>
2017-11-14 09:22:52 -0300

    Create Menu.cpp

---
+++ Code/Menu.cpp
@@ -0,0 +1,16 @@
+#include "Menu.h"
+#include <iostream>
+
+using namespace std;
+
+void Menu::setMenu(int o){
+     opcao = o;
+}
+
+int Menu::getMenu(){
+     return opcao;
+}
+
+void Menu::MostraMenu(){
+
+}
