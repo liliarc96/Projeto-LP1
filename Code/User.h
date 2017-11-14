a18cbeb95611ddfa3bfe3567bafcf839864be563
Rebeca Medeiros <rebeca.andrade.medeiros@gmail.com>
2017-11-14 11:04:57 -0300

    Create User.h

---
+++ Code/User.h
@@ -0,0 +1,15 @@
+#ifndef USER_H
+#define USER_H
+#include "Record.h"
+#include <iostream>
+
+class User: public Record{
+  private:
+    std::string nome;
+  public:
+    void setNome(std::string n);
+    std::string getNome();
+};
+
+
+#endif // USER_H
