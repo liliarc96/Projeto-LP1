0c0ae501531042028ddbec5d9a63e8d742842267
Rebeca Medeiros <rebeca.andrade.medeiros@gmail.com>
2017-11-14 10:58:42 -0300

    Create Jogo.cpp

---
+++ Code/Jogo.cpp
@@ -0,0 +1,34 @@
+#include "Record.h"
+#include <iostream>
+// adicionar include de arquivo
+
+using namespace std;
+
+void Record::setPontuacao(int p){
+    pontuacao = p;
+}
+
+int Record::getPontuacao(){
+    return pontuacao;
+}
+
+void Record::setCor(){
+    //adicionar random para escolher cor
+}
+
+string Record::getCor(){
+    return cor;
+}
+
+void Record::iniciarJogo(){
+    // chamar opencv
+}
+
+int Record::encerrarJogo(){
+    // terminar jogo e exibir pontuacao
+}
+
+void Record::processamentoDeImagens(){
+    //receber imagem da classe opencv e reconhecer 
+}
+
