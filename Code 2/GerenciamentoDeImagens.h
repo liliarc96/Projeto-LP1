92d6e210c4970fc067146a480c67a9e9f88ba4a3
Rebeca Medeiros <rebeca.andrade.medeiros@gmail.com>
2017-11-23 11:33:53 -0300

    Add files via upload

--- Code 2/GerenciamentoDeImagens.h
+++ Code 2/GerenciamentoDeImagens.h
@@ -1,1 +1,15 @@
+#ifndef GERENCIAMENTODEIMAGENS_H
+#define GERENCIAMENTODEIMAGENS_H
+#include <iostream>
 
+class GerenciamentoDeImagens{
+    private:
+        std::string cor;
+    public:
+        void setCor(std::string c);
+        std::string getCor();
+
+        void ProcessamentoDeImagens();
+};
+
+#endif // GERENCIAMENTODEIMAGENS_H
