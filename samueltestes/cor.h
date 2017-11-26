b69c87669d4026dc4e77267c09f7a4a09073a056
Samuel Luiz <30871470+samuelluiz@users.noreply.github.com>
2017-11-26 16:32:21 -0300

    Create cor.h

---
+++ samueltestes/cor.h
@@ -0,0 +1,15 @@
+#ifndef COR_H
+#define COR_H
+
+class cor
+{
+    public:
+        cor();
+        cor(int i);
+        virtual ~cor();
+    protected:
+    private:
+        
+};
+
+#endif // COR_H
