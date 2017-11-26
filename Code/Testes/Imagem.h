1b6e1e69125ba85fba716eb12b3767e145c44180
Rebeca Medeiros <rebeca.andrade.medeiros@gmail.com>
2017-11-26 15:26:58 -0200

    Add files via upload

---
+++ Code/Testes/Imagem.h
@@ -0,0 +1,9 @@
+#ifndef IMAGEM_H
+#define IMAGEM_H
+
+class Imagem{
+    public:
+        int reconhecerCor();
+};
+
+#endif // IMAGEM_H
--- Code/Testes/mainteste.cpp
+++ Code/Testes/mainteste.cpp
@@ -1,1 +1,14 @@
+#include "Imagem.h"
+#include <iostream>
 
+using namespace std;
+
+int main() {
+  Imagem ig;
+
+  cout << "       Bem-vindo ao Jogo das Cores" << endl;
+
+  ig.reconhecerCor();
+
+  return 0;
+}
