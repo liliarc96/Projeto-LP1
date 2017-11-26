cd88bad2da682a3d5af962964cadc4488ae39a06
Samuel Luiz <30871470+samuelluiz@users.noreply.github.com>
2017-11-26 16:35:20 -0300

    Create geracor.h

---
+++ samueltestes/geracor.h
@@ -0,0 +1,15 @@
+#ifndef GERACOR_H
+#define GERACOR_H
+
+
+class geracor
+{
+    public:
+        geracor();
+        virtual ~geracor();
+        void cor();
+    protected:
+    private:
+};
+
+#endif // GERACOR_H
