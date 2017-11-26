41dd60fe582ca7357fb068450c4e461b86bdfa9c
Samuel Luiz <30871470+samuelluiz@users.noreply.github.com>
2017-11-26 16:32:49 -0300

    Create cor.cpp

---
+++ samueltestes/cor.cpp
@@ -0,0 +1,28 @@
+#include "cor.h"
+
+cor::cor()
+{
+    //ctor
+}
+
+cor::cor(int i){
+    switch (i){
+    case 0:
+        //mostra retangulo amarela
+    case 1:
+        //mostra retangulo azul
+    case 2:
+        //mostra retangulo branco
+    case 3:
+        //mostra retangulo preto
+    case 4:
+        //mostra retangulo verde
+    case 5:
+        //mostra retangulo vermelho
+    }
+}
+
+cor::~cor()
+{
+    //dtor
+}
